f37531912d54000f2c55160d04bae69b03b61baf
Djumabaevs <djumabaevb@gmail.com>
2021-10-01 09:46:20 +0600

    fragment maps binding

--- app/src/main/java/com/djumabaevs/distancetracker/MapsFragment.kt
+++ app/src/main/java/com/djumabaevs/distancetracker/MapsFragment.kt
@@ -14,34 +14,21 @@ import com.google.android.gms.maps.SupportMapFragment
 import com.google.android.gms.maps.model.LatLng
 import com.google.android.gms.maps.model.MarkerOptions
 
-class MapsFragment : Fragment() {
+class MapsFragment : Fragment(), OnMapReadyCallback, GoogleMap.OnMyLocationButtonClickListener,
+    GoogleMap.OnMarkerClickListener, EasyPermissions.PermissionCallbacks {
 
-    private val callback = OnMapReadyCallback { googleMap ->
-        /**
-         * Manipulates the map once available.
-         * This callback is triggered when the map is ready to be used.
-         * This is where we can add markers or lines, add listeners or move the camera.
-         * In this case, we just add a marker near Sydney, Australia.
-         * If Google Play services is not installed on the device, the user will be prompted to
-         * install it inside the SupportMapFragment. This method will only be triggered once the
-         * user has installed Google Play services and returned to the app.
-         */
-        val sydney = LatLng(-34.0, 151.0)
-        googleMap.addMarker(MarkerOptions().position(sydney).title("Marker in Sydney"))
-        googleMap.moveCamera(CameraUpdateFactory.newLatLng(sydney))
-    }
+    private var _binding: FragmentMapsBinding? = null
+    private val binding get() = _binding!!
 
-    override fun onCreateView(
-        inflater: LayoutInflater,
-        container: ViewGroup?,
-        savedInstanceState: Bundle?
-    ): View? {
-        return inflater.inflate(R.layout.fragment_maps, container, false)
-    }
+    private lateinit var map: GoogleMap
 
-    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
-        super.onViewCreated(view, savedInstanceState)
-        val mapFragment = childFragmentManager.findFragmentById(R.id.map) as SupportMapFragment?
-        mapFragment?.getMapAsync(callback)
-    }
-}
+    val started = MutableLiveData(false)
+
+    private var startTime = 0L
+    private var stopTime = 0L
+
+    private var locationList = mutableListOf<LatLng>()
+    private var polylineList = mutableListOf<Polyline>()
+    private var markerList = mutableListOf<Marker>()
+
+    private lateinit var fusedLocationProviderClient: FusedLocationProviderClient
