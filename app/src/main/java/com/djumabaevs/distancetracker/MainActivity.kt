b1d3428f7586a79f0b69307c66374f9b9c6713d0
Djumabaevs <djumabaevb@gmail.com>
2021-10-01 09:45:46 +0600

    map util and binding fragment

--- app/src/main/java/com/djumabaevs/distancetracker/MainActivity.kt
+++ app/src/main/java/com/djumabaevs/distancetracker/MainActivity.kt
@@ -2,10 +2,24 @@ package com.djumabaevs.distancetracker
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.navigation.NavController
+import androidx.navigation.fragment.NavHostFragment
+import com.djumabaevs.distancetracker.Permissions.hasLocationPermission
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var navController: NavController
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val navHostFragment = supportFragmentManager
+            .findFragmentById(R.id.navHostFragment) as NavHostFragment
+        navController = navHostFragment.navController
+
+        if (hasLocationPermission(this)) {
+            navController.navigate(R.id.action_permissionFragment_to_mapsFragment)
+        }
+
     }
 }
