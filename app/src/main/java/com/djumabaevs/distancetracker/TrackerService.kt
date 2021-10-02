58615719a9ad51eb776abe8f3d2541b166c76774
Djumabaevs <djumabaevb@gmail.com>
2021-10-02 10:19:29 +0600

    companion object with mutable live data

--- app/src/main/java/com/djumabaevs/distancetracker/TrackerService.kt
+++ app/src/main/java/com/djumabaevs/distancetracker/TrackerService.kt
@@ -1,7 +1,13 @@
 package com.djumabaevs.distancetracker
 
+import android.app.NotificationManager
+import androidx.core.app.NotificationCompat
 import androidx.lifecycle.LifecycleService
+import androidx.lifecycle.MutableLiveData
+import com.google.android.gms.location.FusedLocationProviderClient
+import com.google.android.gms.maps.model.LatLng
 import dagger.hilt.android.AndroidEntryPoint
+import javax.inject.Inject
 
 @AndroidEntryPoint
 class TrackerService : LifecycleService() {
@@ -13,6 +19,14 @@ class TrackerService : LifecycleService() {
     lateinit var notificationManager: NotificationManager
 
     private lateinit var fusedLocationProviderClient: FusedLocationProviderClient
+
+    companion object {
+        val started = MutableLiveData<Boolean>()
+        val startTime = MutableLiveData<Long>()
+        val stopTime = MutableLiveData<Long>()
+
+        val locationList = MutableLiveData<MutableList<LatLng>>()
+    }
 
 
 
