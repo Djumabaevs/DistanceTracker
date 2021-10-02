752057dd37e61c1c8381eb55f5c597a7a697efc4
Djumabaevs <djumabaevb@gmail.com>
2021-10-02 10:13:34 +0600

    provide pending intent in notification module

--- app/src/main/java/com/djumabaevs/distancetracker/NotificationModule.kt
+++ app/src/main/java/com/djumabaevs/distancetracker/NotificationModule.kt
@@ -1,2 +1,19 @@
 package com.djumabaevs.distancetracker
 
+@Module
+@InstallIn(ServiceComponent::class)
+object NotificationModule {
+
+    @ServiceScoped
+    @Provides
+    fun providePendingIntent(
+        @ApplicationContext context: Context
+    ): PendingIntent {
+        return PendingIntent.getActivity(
+            context,
+            PENDING_INTENT_REQUEST_CODE,
+            Intent(context, MainActivity::class.java),
+            PendingIntent.FLAG_UPDATE_CURRENT
+        )
+    }
+
